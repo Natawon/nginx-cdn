699e444a5843f6afa212507c83229bd8808aa064
Natawon Patarasundorn <natawon9@gmail.com>
2019-01-22 12:16:12 +0700

    add update_vnstat

---
+++ update-vnstat.sh
@@ -0,0 +1,8 @@
+vnstati -s -i ens160 -o /docker/nginx-cdn/data/www/vnstat/vnstat-s.png
+vnstati -m -i ens160 -o /docker/nginx-cdn/data/www/vnstat/vnstat-m.png
+vnstati -d -i ens160 -o /docker/nginx-cdn/data/www/vnstat/vnstat-d.png
+vnstati -h -i ens160 -o /docker/nginx-cdn/data/www/vnstat/vnstat-h.png
+vnstati -vs -i ens160 -o /docker/nginx-cdn/data/www/vnstat/vnstat-vs.png
+vnstati -hs -i ens160 -o /docker/nginx-cdn/data/www/vnstat/vnstat-hs.png
+
+
