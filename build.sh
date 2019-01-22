9ac9c02a949dbb7b9b7d409fab3ae738a8023f52
Natawon <32977030+Natawon@users.noreply.github.com>
2019-01-22 11:06:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #docker stop $(docker ps -a -q)
 #docker rm $(docker ps -a -q)
 
-docker stop nginx-cdn; docker rm nginx-cdn;
+#docker stop nginx-cdn; docker rm nginx-cdn;
 docker run --restart=always --name nginx-cdn -p 80:80  \
 -v $PWD/config/etc/nginx/conf.d:/etc/nginx/conf.d \
 -v $PWD/config/etc/nginx/nginx.conf:/etc/nginx/nginx.conf \
